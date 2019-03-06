3387f16f9d5071cdcf2f61925d10d045f9c8d254
beztormoza <beztormoza@ukr.net>
2019-03-06 14:09:13 +0200

    created tests

---
+++ test/studentPlanTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class studentPlanTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void closeCheck__withOneProduct() {
+//        assertThat(smth, is());
+    }
+
+}
