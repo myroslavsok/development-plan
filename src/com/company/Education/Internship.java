5a83af02642c5bce7994257d1e278789d9379dc1
beztormoza <beztormoza@ukr.net>
2019-03-07 11:17:32 +0200

    Create KnowledgeSource Internship

--- src/com/company/Education/Internship.java
+++ src/com/company/Education/Internship.java
@@ -4,9 +4,19 @@ import com.company.Student;
 
 public class Internship implements KnowledgeSource {
 
+    private double knowledgeLevel;
+    private double experienceLevel;
+
+    public Internship(int knowledge, int experience) {
+        this.knowledgeLevel = knowledge;
+        this.experienceLevel = experience;
+    }
+
     @Override
     public void giveKnowledge(Student student) {
-
+        if (student.getIsInInternship()) {
+            student.improveSkills(this.knowledgeLevel, this.experienceLevel);
+        }
     }
 
 }
